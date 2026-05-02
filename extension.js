24a5826b9e6265262adad5c99092b8ecdc7a9cf7
Mihiran <85764123+kavindamihiran@users.noreply.github.com>
2026-05-03 03:26:46 +0530

    refactor: migrate extension to ESM class-based structure and improve focus locking reliability

--- extension.js
+++ extension.js
@@ -11,38 +11,70 @@ import Shell from 'gi://Shell';
 import Clutter from 'gi://Clutter';
 import GLib from 'gi://GLib';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 
 const UNLOCKED_ICON = '◎';  // Open circle - green
 const LOCKED_ICON = '◉';    // Filled circle - red
 
-const FocusLockIndicator = GObject.registerClass(
-class FocusLockIndicator extends PanelMenu.Button {
-    _init() {
-        super._init(0.0, 'Focus Lock Indicator');
-
-        // State
+export default class FocusLockExtension extends Extension {
+    constructor(metadata) {
+        super(metadata);
+        this._button = null;
+        this._label = null;
+        this._buttonClickHandlerId = null;
         this._isLocked = false;
         this._lockedWindow = null;
         this._focusHandlerId = null;
         this._windowCreatedId = null;
         this._focusTimeoutId = null;
         this._minimizeTimeoutId = null;
+        this._unmanagedId = null;
+    }
 
-        // Create the label for the panel
+    enable() {
+        // Create a simple St.Button and add it to the panel
+        this._button = new St.Button({
+            reactive: true,
+            can_focus: false,
+            track_hover: true,
+            style_class: 'panel-button focus-lock-indicator unlocked',
+        });
+
         this._label = new St.Label({
             text: UNLOCKED_ICON,
             y_align: Clutter.ActorAlign.CENTER,
-            style_class: 'focus-lock-indicator unlocked'
         });
-        this.add_child(this._label);
 
-        // Connect click handler
-        this.connect('button-press-event', () => {
+        this._button.set_child(this._label);
+
+        this._buttonClickHandlerId = this._button.connect('clicked', () => {
             this._toggleLock();
-            return Clutter.EVENT_STOP;
         });
+
+        // Add to the right side of the panel
+        Main.panel._rightBox.insert_child_at_index(this._button, 0);
+    }
+
+    disable() {
+        this._unlock();
+
+        if (this._buttonClickHandlerId) {
+            if (this._button) {
+                this._button.disconnect(this._buttonClickHandlerId);
+            }
+            this._buttonClickHandlerId = null;
+        }
+
+        if (this._label) {
+            this._label.destroy();
+            this._label = null;
+        }
+
+        if (this._button) {
+            Main.panel._rightBox.remove_child(this._button);
+            this._button.destroy();
+            this._button = null;
+        }
     }
 
     _toggleLock() {
@@ -54,9 +86,8 @@ class FocusLockIndicator extends PanelMenu.Button {
     }
 
     _lock() {
-        // Get the currently focused window
         const focusedWindow = global.display.get_focus_window();
-        
+
         if (!focusedWindow) {
             Main.notify('Focus Lock', 'No window is currently focused!');
             return;
@@ -67,16 +98,23 @@ class FocusLockIndicator extends PanelMenu.Button {
 
         // Update UI
         this._label.text = LOCKED_ICON;
-        this._label.style_class = 'focus-lock-indicator locked';
+        this._button.style_class = 'panel-button focus-lock-indicator locked';
 
         // Connect to focus changes
         this._focusHandlerId = global.display.connect('notify::focus-window', () => {
             this._onFocusChanged();
         });
 
-        // Connect to new window creation - minimize any new windows
-        this._windowCreatedId = global.display.connect('window-created', (display, window) => {
+        // Connect to new window creation
+        this._windowCreatedId = global.display.connect('window-created', (_display, window) => {
             this._onWindowCreated(window);
+        });
+
+        // Track if the locked window is closed
+        this._unmanagedId = this._lockedWindow.connect('unmanaged', () => {
+            if (this._isLocked) {
+                this._unlock();
+            }
         });
 
         // Get app name for notification
@@ -86,139 +124,122 @@ class FocusLockIndicator extends PanelMenu.Button {
     }
 
     _unlock() {
-        // Disconnect focus handler
         if (this._focusHandlerId) {
             global.display.disconnect(this._focusHandlerId);
             this._focusHandlerId = null;
         }
 
-        // Disconnect window created handler
         if (this._windowCreatedId) {
             global.display.disconnect(this._windowCreatedId);
             this._windowCreatedId = null;
         }
-        
-        // Clear timeouts
+
+        if (this._unmanagedId && this._lockedWindow) {
+            try {
+                this._lockedWindow.disconnect(this._unmanagedId);
+            } catch (_e) {
+                // Window may already be destroyed
+            }
+            this._unmanagedId = null;
+        }
+
         if (this._focusTimeoutId) {
             GLib.source_remove(this._focusTimeoutId);
             this._focusTimeoutId = null;
         }
-        
+
         if (this._minimizeTimeoutId) {
             GLib.source_remove(this._minimizeTimeoutId);
             this._minimizeTimeoutId = null;
         }
 
+        const wasLocked = this._isLocked;
         this._lockedWindow = null;
         this._isLocked = false;
 
         // Update UI
-        this._label.text = UNLOCKED_ICON;
-        this._label.style_class = 'focus-lock-indicator unlocked';
+        if (this._label) {
+            this._label.text = UNLOCKED_ICON;
+        }
+        if (this._button) {
+            this._button.style_class = 'panel-button focus-lock-indicator unlocked';
+        }
 
-        Main.notify('Focus Lock', 'Unlocked - you can now switch windows');
+        if (wasLocked) {
+            Main.notify('Focus Lock', 'Unlocked - you can now switch windows');
+        }
     }
 
-    _onFocusChanged() {
-        // If we're locked and focus changed away from our window, refocus it
+    _refocusLockedWindow() {
         if (!this._isLocked || !this._lockedWindow) {
             return;
         }
 
-        const currentFocus = global.display.get_focus_window();
-        
-        // If focus moved to a different window, minimize it and bring back our locked window
-        if (currentFocus && currentFocus !== this._lockedWindow) {
-            // Check if the locked window still exists
-            if (this._lockedWindow.get_compositor_private()) {
-                // Minimize the intruding window
-                if (currentFocus.can_minimize()) {
-                    currentFocus.minimize();
-                }
-                
-                // Use a small timeout to avoid focus race conditions
-                if (this._focusTimeoutId) {
-                    GLib.source_remove(this._focusTimeoutId);
-                }
-                
-                this._focusTimeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 50, () => {
-                    this._focusTimeoutId = null;
-                    if (this._isLocked && this._lockedWindow) {
-                        const timestamp = global.get_current_time();
-                        this._lockedWindow.activate(timestamp);
-                    }
-                    return GLib.SOURCE_REMOVE;
-                });
-            } else {
-                // Window was closed, unlock
-                this._unlock();
-            }
-        }
-    }
-
-    _onWindowCreated(window) {
-        // Minimize any new window that opens while locked
-        if (!this._isLocked || !this._lockedWindow) {
-            return;
+        if (this._focusTimeoutId) {
+            GLib.source_remove(this._focusTimeoutId);
         }
 
-        // Wait a bit for the window to be ready, then minimize it
-        if (this._minimizeTimeoutId) {
-            GLib.source_remove(this._minimizeTimeoutId);
-        }
-
-        this._minimizeTimeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 100, () => {
-            this._minimizeTimeoutId = null;
-            if (this._isLocked && window && window !== this._lockedWindow) {
-                if (window.can_minimize()) {
-                    window.minimize();
+        this._focusTimeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 100, () => {
+            this._focusTimeoutId = null;
+            if (this._isLocked && this._lockedWindow) {
+                const activeWorkspace = global.workspace_manager.get_active_workspace();
+                if (!this._lockedWindow.located_on_workspace(activeWorkspace)) {
+                    this._lockedWindow.change_workspace(activeWorkspace);
                 }
-                // Refocus our locked window
-                if (this._lockedWindow) {
-                    const timestamp = global.get_current_time();
-                    this._lockedWindow.activate(timestamp);
+                if (this._lockedWindow.minimized) {
+                    this._lockedWindow.unminimize();
                 }
+                this._lockedWindow.activate(global.get_current_time());
             }
             return GLib.SOURCE_REMOVE;
         });
     }
 
-    destroy() {
-        if (this._focusHandlerId) {
-            global.display.disconnect(this._focusHandlerId);
-            this._focusHandlerId = null;
+    _onFocusChanged() {
+        if (!this._isLocked || !this._lockedWindow) {
+            return;
         }
-        if (this._windowCreatedId) {
-            global.display.disconnect(this._windowCreatedId);
-            this._windowCreatedId = null;
+
+        const currentFocus = global.display.get_focus_window();
+
+        if (currentFocus && currentFocus !== this._lockedWindow) {
+            const windowType = currentFocus.get_window_type();
+            if (windowType === Meta.WindowType.MENU ||
+                windowType === Meta.WindowType.DROPDOWN_MENU ||
+                windowType === Meta.WindowType.POPUP_MENU ||
+                windowType === Meta.WindowType.MODAL_DIALOG) {
+                return;
+            }
+
+            if (currentFocus.can_minimize()) {
+                currentFocus.minimize();
+            }
+
+            this._refocusLockedWindow();
         }
-        if (this._focusTimeoutId) {
-            GLib.source_remove(this._focusTimeoutId);
-            this._focusTimeoutId = null;
+    }
+
+    _onWindowCreated(window) {
+        if (!this._isLocked || !this._lockedWindow) {
+            return;
         }
+
         if (this._minimizeTimeoutId) {
             GLib.source_remove(this._minimizeTimeoutId);
+        }
+
+        this._minimizeTimeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 200, () => {
             this._minimizeTimeoutId = null;
-        }
-        super.destroy();
-    }
-});
-
-export default class FocusLockExtension extends Extension {
-    constructor(metadata) {
-        super(metadata);
-        this._indicator = null;
-    }
-
-    enable() {
-        this._indicator = new FocusLockIndicator();
-        Main.panel.addToStatusArea('focus-lock-indicator', this._indicator);
-    }
-
-    disable() {
-        if (this._indicator) {
-            this._indicator.destroy();
-            this._indicator = null;
-        }
+            if (this._isLocked && window && window !== this._lockedWindow) {
+                if (window.get_window_type() !== Meta.WindowType.NORMAL) {
+                    return GLib.SOURCE_REMOVE;
+                }
+                if (window.can_minimize()) {
+                    window.minimize();
+                }
+                this._refocusLockedWindow();
+            }
+            return GLib.SOURCE_REMOVE;
+        });
     }
 }
